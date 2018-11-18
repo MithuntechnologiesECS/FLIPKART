68cc50533f77f9bde4cf7a0e153b517a1e4c1774
suryadattaharish <suryavaddadi1@gmail.com>
2018-11-18 12:19:06 +0530

    my first commit

---
+++ hr.java
@@ -0,0 +1,3 @@
+ hey this is hrithik roshan 
+ the greek god and the dancing King of INDIA
+ have a nice day 
---
+++ mb.java
@@ -0,0 +1,4 @@
+
+hey this is Superstar mahesh
+if ur target is 9th mile aim for the 10th mile
+all the best fellas
